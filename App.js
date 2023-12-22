c9197f3824ccd023d64501211e64fecd65d104d5
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-12-21 21:31:32 -0500

    added google maps on main screen

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        provider={PROVIDER_GOOGLE} // If you want to use Google Maps
+        style={styles.map}
+        region={{
+          latitude: 42.9634,  // Latitude for Grand Rapids
+          longitude: -85.6681, // Longitude for Grand Rapids
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
