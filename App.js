03beb2d2854457a54c9ed355956942c5042a156e
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-01-06 15:52:26 -0500

    made navigation functional

--- App.js
+++ App.js
@@ -1,80 +1,18 @@
-//App.js
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
 
+import Navigation from './Navigation'; 
+/**
+ * Root component for the React Native application for CalvinFinds.
+ * This component serves as the entry point for the app, rendering the main navigation structure.
+ * The app is designed for tracking lost and found items, allowing users to
+ * report and discover lost or found items within the Calvin community.
+ * @returns {JSX.Element} - JSX representation of the root App component.
+ * 
+ * @author: CalvinFinds Team
+ * @date: Fall, 2023
+ */
 export default function App() {
-  // const navigation = useNavigation()
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.secondaryButton} onPress={() => navigation.navigate('Menu')}>
-        <Text style={styles.buttonText}>Find Resources</Text>
-      </TouchableOpacity>
-      <MapView 
-        provider={PROVIDER_GOOGLE} // If you want to use Google Maps
-        style={styles.map}
-        mapType="standard" // Do not use satellite view
-        region={{
-          latitude: 42.9634,  // Latitude for Grand Rapids
-          longitude: -85.6681, // Longitude for Grand Rapids
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-      <TouchableOpacity style={styles.primaryButton} >
-        <Text style={styles.buttonText}>Call Navigator</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+  return (<Navigation />);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  primaryButton: {
-    alignItems: 'center',
-    backgroundColor: '#A33636',
-    borderRadius: 50,
-    width: '80%',
-    padding: 18,
-    marginVertical: 20,
-    position: 'absolute',
-    bottom: 10,
-    shadowColor: '#A59D95',
-    shadowOffset: {width: 0, height: 8},
-    shadowOpacity: 0.2,
-    shadowRadius: 24,
-    elevation: 7,
-    zIndex: 11,
-  },
-  secondaryButton: {
-    alignItems: 'center',
-    backgroundColor: '#222',
-    borderRadius: 50,
-    width: '85%',
-    padding: 18,
-    marginVertical: 20,
-    position: 'absolute',
-    top: 30,
-    shadowColor: '#A59D95',
-    shadowOffset: {width: 0, height: 8},
-    shadowOpacity: 0.2,
-    shadowRadius: 24,
-    elevation: 7,
-    zIndex: 11,
-  },
-  buttonText: {
-    fontWeight: '700',
-    fontSize: 20,
-    color: '#fff',
-  },
-});
--- Navigation.js
+++ Navigation.js
@@ -1,19 +1,18 @@
 // Navigation.js
 import React from 'react';
-import {StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MainPage from "./App";
-import Menu from "./Menu";
+import MainPage from './MainPage'; // Assuming MainPage is your App.js
+import Menu from './Menu';
 
 const Stack = createStackNavigator();
 
 function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainPage">
-        <Stack.Screen name="MainPage" component={MainPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Menu" component={Menu} /> {/* Add Menu Screen */}
+      <Stack.Navigator>
+        <Stack.Screen name="MainPage" component={MainPage} />
+        <Stack.Screen name="Menu" component={Menu} />
       </Stack.Navigator>
     </NavigationContainer>
   );
