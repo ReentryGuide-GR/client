0781852d89dd898dfb791066309af5e332c30c3d
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-02-20 22:17:26 -0500

    moved functions to utils.js

---
+++ utils.js
@@ -0,0 +1,60 @@
+// utils.js
+import * as Location from 'expo-location';
+import { Alert, Linking, Platform } from 'react-native';
+import locations from './locationsData'; // Ensure this path matches the location of your locations data file
+
+// Haversine formula to calculate distance between two coordinates
+export const getDistance = (lat1, lng1, lat2, lng2) => {
+  const toRad = x => (x * Math.PI) / 180;
+  const R = 6371; // Earth radius in km
+  const dLat = toRad(lat2 - lat1);
+  const dLng = toRad(lng2 - lng1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+            Math.sin(dLng / 2) * Math.sin(dLng / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+};
+
+// Function to find the closest location from a category
+export const findClosestLocation = async (category) => {
+  let userLocation = await getUserLocation();
+  if (!userLocation) {
+    Alert.alert("Error", "Unable to get user location");
+    return null;
+  }
+
+  const closest = locations[category].reduce((prev, curr) => {
+    const prevDistance = getDistance(userLocation.latitude, userLocation.longitude, prev.lat, prev.lng);
+    const currDistance = getDistance(userLocation.latitude, userLocation.longitude, curr.lat, curr.lng);
+    return (prevDistance < currDistance) ? prev : curr;
+  });
+
+  return closest; // Return the closest location object
+};
+
+// Function to get the user's current location
+export const getUserLocation = async () => {
+  try {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permission Denied', 'Permission to access location was denied');
+      return null;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    return location.coords;
+  } catch (error) {
+    Alert.alert('Location Error', "Couldn't get location");
+    return null;
+  }
+};
+
+// Function to open Google Maps with given coordinates
+export const openGoogleMaps = (lat, lng) => {
+  const url = Platform.OS === 'android'
+    ? `google.navigation:q=${lat},${lng}&mode=w` // 'w' stands for walking
+    : `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}&travelmode=walking`;
+
+  Linking.openURL(url).catch(err => Alert.alert("Error", "Couldn't load page"));
+};
