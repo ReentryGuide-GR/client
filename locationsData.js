b701b9209e49a69353e4d5a8f52a0f9591ec039f
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-02-24 14:50:13 -0500

    added some groceries data

--- locationsData.js
+++ locationsData.js
@@ -4,8 +4,15 @@ const locations = {
       { lat: 42.959223, lng: -85.669267, name: "Mel Trotter Ministries" },
       { lat: 42.8462516, lng: -85.6206727, name: "Meijer" },
 
-      // Add more food locations
+      // Add more meal locations
     ],
+    Groceries: [
+      { lat: 42.935567, lng: -85.699914, name: "United Church Outreach Ministry" },
+      { lat: 42.95332, lng: -85.64876, name: "Eastern Avenue CRC" },
+
+      // Add more meal locations
+    ],
+
     Clothing: [
       { lat: 42.9612, lng: -85.6665, name: "Clothing Store 1" },
       // Add more clothing locations
