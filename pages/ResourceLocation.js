5040528c525f905fdd856ec5953f32ea9a2bf6f6
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-03-02 16:38:27 -0500

    allow navigate to Transportation.js

--- pages/ResourceLocation.js
+++ pages/ResourceLocation.js
@@ -14,6 +14,7 @@ import { openGoogleMaps } from '../utils'
 
 
 const ResourceLocation = ({ isVisible, onClose }) => {
+  const navigation = useNavigation(); // used for navigation.navigate()
 
   const route = useRoute();
   const { location } = route.params;
@@ -49,7 +50,7 @@ return (
               imageSource={require('../assets/directions.png')}
               title="Plan Your Route  "
               buttonStyle={styles.secondaryButton}
-              onPress={handlePlanYourRoute}
+              onPress={() => navigation.navigate('Transportation')}
             />
 
             <ActionButton
