36bc30fdfe5781993d2a6bae99c72e5da63de95b
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-03-31 21:42:52 -0400

    implemented icons

--- pages/SelectResourceLocation.js
+++ pages/SelectResourceLocation.js
@@ -37,12 +37,14 @@ const SelectResourceLocation = ({ isVisible, onClose }) => {
       <View style={styles.resourceContainer}>
         <Text style={styles.title}>Select {category} Location</Text>
         <ActionButton
+          imageSource={require('../assets/bullseye.png')}
           title="Select Closest Location"
           buttonStyle={styles.secondaryButton}
           onPress={handleSelectClosestLocation}
         />
 
         <ActionButton
+          imageSource={require('../assets/locations.png')}
           title="Select Other Locations"
           onPress={onClose}
           buttonStyle={styles.tertiaryButton}
