010bd2d00f25f34cd1c0535b9d964514b2bddb67
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-03 16:00:08 -0400

    Added Transitional Animation

--- Navigation.js
+++ Navigation.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import MainPage from './Menu'; 
 import SelectResourceLocation from './pages/SelectResourceLocation'; 
 import MealOrGroceries from './pages/MealOrGroceries'; 
@@ -14,12 +14,19 @@ const Stack = createStackNavigator();
 function Navigation() {
   return (
     <NavigationContainer>
-        <StatusBar
-            backgroundColor="#fff"
-            barStyle="dark-content"
-            // color="#000"
-        />
-      <Stack.Navigator initialRouteName="MainPage" screenOptions={{ headerShown: false }}>
+      <StatusBar
+          backgroundColor="#fff"
+          barStyle="dark-content"
+          // color="#000"
+      />
+      <Stack.Navigator
+        initialRouteName="MainPage"
+        screenOptions={{
+          headerShown: false,
+          // Customizing the screen transition animation
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
         <Stack.Screen name="MainPage" component={MainPage} />
         <Stack.Screen name="SelectResourceLocation" component={SelectResourceLocation} />
         <Stack.Screen name="MealOrGroceries" component={MealOrGroceries} />
