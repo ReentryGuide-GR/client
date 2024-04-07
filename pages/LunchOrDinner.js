3bd2ee777ab21b1135bd62dc0efb7f0537fadd4c
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-07 14:26:50 -0400

    made LunchOrDinner.js functional

--- pages/LunchOrDinner.js
+++ pages/LunchOrDinner.js
@@ -1,16 +1,43 @@
 // This Page is developed to fit Grand Rapids environment, where 
 // "God’s Kitchen" is for lunch and "Mel Trotter" is for dinner.
 // These two locations are the only locations we know that doesn't require payments for food.  
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Modal, TouchableOpacity, Image, Linking} from 'react-native';
 import { useNavigation, useRoute} from '@react-navigation/native';
 import GoBackButton from '../components/GoBackButton';
 import IconButton from '../components/IconButton';
 import { useFonts } from 'expo-font';
+import { getUserLocation, getDistance } from '../utils';
+import locationsBasic from '../database/locations_basic.json';
 
 
 const Page = ({ onClose }) => {
   const navigation = useNavigation(); // used for navigation.navigate()
+  const [godsKitchenDistance, setGodsKitchenDistance] = useState(null);
+  const [melTrotterDistance, setMelTrotterDistance] = useState(null);
+
+  
+
+  useEffect(() => {
+    (async () => {
+      const userLocation = await getUserLocation();
+      if (!userLocation) {
+        console.log("Could not fetch user location.");
+        return;
+      }
+
+      const melTrotterLocation = locationsBasic.Meal.find(location => location.id === "2");
+      const godsKitchenLocation = locationsBasic.Meal.find(location => location.id === "1");
+
+      const distanceToMelTrotter = getDistance(userLocation.latitude, userLocation.longitude, melTrotterLocation.coordinates.lat, melTrotterLocation.coordinates.lng);
+      const distanceToGodsKitchen = getDistance(userLocation.latitude, userLocation.longitude, godsKitchenLocation.coordinates.lat, godsKitchenLocation.coordinates.lng);
+
+      // Choose which location you want to calculate distance for and set it
+      // For example, if this screen is meant for deciding on dinner:
+      setMelTrotterDistance((distanceToMelTrotter * 0.621371).toFixed(1)); // Convert km to miles and round to 1 decimal place
+      setGodsKitchenDistance((distanceToGodsKitchen * 0.621371).toFixed(1)); // Convert km to miles and round to 1 decimal place
+    })();
+  }, []);
 
   const [fontsLoaded] = useFonts({
     'Manrope-SemiBold': require('../assets/fonts/Manrope-SemiBold.ttf'),
@@ -29,24 +56,43 @@ return (
           <View style={styles.resourceContainer}>
             <Text style={styles.title}>Lunch or Dinner?</Text>
 
-
-            <TouchableOpacity style={styles.IconButton} onPress={() => navigation.navigate('SelectResourceLocation', { category: 'Meal' })}>
+            <TouchableOpacity
+              style={styles.IconButton}
+              onPress={() => {
+                const godsKitchen = locationsBasic.Meal.find(location => location.id === "1");
+                navigation.navigate('ResourceLocation', {
+                  location: godsKitchen,
+                  category: "Meal",
+                  distance: godsKitchenDistance,
+                });
+              }}
+            >
               <View style={styles.row}>
                 <Text style={styles.IconButtonText}>Lunch</Text>
-                <Text style={styles.IconButtonTextBold}>12:30</Text>
+                <Text style={styles.IconButtonTextBold}>4:00</Text>
                 <Text style={styles.IconButtonText}>pm -</Text>
-                <Text style={styles.IconButtonTextBold}>2:00</Text>
+                <Text style={styles.IconButtonTextBold}>5:45</Text>
                 <Text style={styles.IconButtonText}>pm</Text>
               </View>
               <Image source={ require('../assets/arrow_forward.png') } style={[styles.arrow]} />
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.IconButton} onPress={() => navigation.navigate('SelectResourceLocation', { category: 'Meal' })}>
+            <TouchableOpacity
+              style={styles.IconButton}
+              onPress={() => {
+                const melTrotter = locationsBasic.Meal.find(location => location.id === "2"); // Assuming "2" is Mel Trotter
+                navigation.navigate('ResourceLocation', {
+                  location: melTrotter,
+                  category: "Meal",
+                  distance: melTrotterDistance,
+                });
+              }}
+            >
               <View style={styles.row}>
                 <Text style={styles.IconButtonText}>Dinner</Text>
-                <Text style={styles.IconButtonTextBold}>4:00</Text>
+                <Text style={styles.IconButtonTextBold}>12:30</Text>
                 <Text style={styles.IconButtonText}>pm -</Text>
-                <Text style={styles.IconButtonTextBold}>5:45</Text>
+                <Text style={styles.IconButtonTextBold}>2:00</Text>
                 <Text style={styles.IconButtonText}>pm</Text>
               </View>
               <Image source={ require('../assets/arrow_forward.png') } style={[styles.arrow]} />
