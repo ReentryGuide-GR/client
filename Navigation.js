05c8829e677698bf5dc9bdc0a46bf48fae1fb939
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-10 12:45:53 -0400

    Added more info page

--- Navigation.js
+++ Navigation.js
@@ -10,6 +10,7 @@ import LunchOrDinner from './pages/LunchOrDinner';
 import ResourceLocation from './pages/ResourceLocation'; 
 import Transportation from './pages/Transportation'; 
 import LocationsList from './pages/LocationsList'; 
+import MoreInfo from './pages/MoreInfo'; 
 
 const Stack = createStackNavigator();
 
@@ -36,6 +37,7 @@ function Navigation() {
         <Stack.Screen name="ResourceLocation" component={ResourceLocation} />
         <Stack.Screen name="Transportation" component={Transportation} />
         <Stack.Screen name="LocationsList" component={LocationsList} />
+        <Stack.Screen name="MoreInfo" component={MoreInfo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
