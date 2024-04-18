6461c7c11e43d227d668e6ee9f90e803e0c58270
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-18 16:33:16 -0400

    Imported Alert

--- pages/ResourceLocation.js
+++ pages/ResourceLocation.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Modal, TouchableOpacity, Image, Linking} from 'react-native';
+import { StyleSheet, View, Text, Modal, TouchableOpacity, Image, Linking, Alert} from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import IconButton from '../components/IconButton';
