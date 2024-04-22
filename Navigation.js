3dabc2a16c7f9cfec01721155d4ddf18e220785e
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-22 13:23:28 -0400

    fixed navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,18 +1,18 @@
 // Navigation.js
 import React from 'react';
-import {StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-import MainMenu from './pages/MainMenu'; 
-import SelectResourceLocation from './pages/SelectResourceLocation'; 
-import MealOrGroceries from './pages/MealOrGroceries'; 
-import FindHealthcare from './pages/FindHealthcare'; 
-import LunchOrDinner from './pages/LunchOrDinner'; 
-import ResourceLocation from './pages/ResourceLocation'; 
-import Transportation from './pages/Transportation'; 
-import LocationsList from './pages/LocationsList'; 
-import MoreInfo from './pages/MoreInfo'; 
-import RequestPermission from './pages/RequestPermission'; 
+import MainMenu from './pages/MainMenu';
+import SelectResourceLocation from './pages/SelectResourceLocation';
+import MealOrGroceries from './pages/MealOrGroceries';
+import FindHealthcare from './pages/FindHealthcare';
+import LunchOrDinner from './pages/LunchOrDinner';
+import ResourceLocation from './pages/ResourceLocation';
+import Transportation from './pages/Transportation';
+import LocationsList from './pages/LocationsList';
+import MoreInfo from './pages/MoreInfo';
+import RequestPermission from './pages/RequestPermission';
 
 const Stack = createStackNavigator();
 
@@ -20,9 +20,9 @@ function Navigation() {
   return (
     <NavigationContainer>
       <StatusBar
-          backgroundColor="#fff"
-          barStyle="dark-content"
-          // color="#000"
+        backgroundColor="#fff"
+        barStyle="dark-content"
+        // color="#000"
       />
       <Stack.Navigator
         initialRouteName="MainMenu"
