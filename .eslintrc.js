ae74cb25a7710418f300a2cefcfb811f6415ab74
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-22 14:56:29 -0400

    modified eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     node: true,
   },
   extends: 'airbnb',
+  plugins: [
+    '@react-native',
+  ],
   overrides: [
     {
       env: {
@@ -24,5 +27,14 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    // prevent eslint to complain about the "styles" variable being used before it was defined
+    'no-use-before-define': ['error', { variables: false }],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 };
