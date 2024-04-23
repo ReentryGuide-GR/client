6c458fb067e8cb18f505cf9c1e240eccec1ce2fd
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-04-22 21:26:02 -0400

    Cleaned up MealOrGroceries.js

--- pages/MealOrGroceries.js
+++ pages/MealOrGroceries.js
@@ -1,52 +1,44 @@
 import React from 'react';
-import { StyleSheet, View, Text, Modal, TouchableOpacity, Image, Linking} from 'react-native';
-import { useNavigation, useRoute} from '@react-navigation/native';
-import * as Location from 'expo-location';
+import {
+  StyleSheet, View, Text,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+// import * as Location from 'expo-location';
 // import ActionButton from '../components/ActionButton';
 import GoBackButton from '../components/GoBackButton';
 import IconButton from '../components/IconButton';
 // import locationsData from './database/locations_basic.json';
 // import * as styles from '../../styles/detailsStyles';
 
-
-const Page = ({ isVisible, onClose }) => {
+const Page = () => {
   const navigation = useNavigation(); // used for navigation.navigate()
-return (
+  return (
+    <View style={styles.mainContainer}>
+      {/* Empty Component to make buttons in the middle of the screen but not on top,
+      easier for user to reach */}
+      <View />
+      <View style={styles.resourceContainer}>
+        <Text style={styles.title}>Meal or Groceries?</Text>
+        <IconButton
+          imageSource={require('../assets/meal.png')}
+          title="Find Meal"
+          buttonStyle={styles.primaryButton}
+          onPress={() => navigation.navigate('LunchOrDinner')}
+        />
 
-        <View style={styles.mainContainer}>
-          {/* Empty Component to make buttons in the middle of the screen but not on top, easier for user to reach*/}
-          <View></View> 
-          <View style={styles.resourceContainer}>
-            <Text style={styles.title}>Meal or Groceries?</Text>
-            <IconButton
-              imageSource={require('../assets/meal.png')}
-              title="Find Meal"
-              buttonStyle={styles.primaryButton}
-              onPress={() => navigation.navigate('LunchOrDinner')}
-            />
+        <IconButton
+          imageSource={require('../assets/grocery.png')}
+          title="Find Groceries"
+          buttonStyle={styles.primaryButton}
+          onPress={() => navigation.navigate('SelectResourceLocation', { category: 'groceries', title: 'Groceries' })}
+        />
+      </View>
 
-            <IconButton
-              imageSource={require('../assets/grocery.png')}
-              title="Find Groceries"
-              buttonStyle={styles.primaryButton}
-              onPress={() => navigation.navigate('SelectResourceLocation', { category: 'groceries', title: 'Groceries' })}
-            />
-          </View>
+      <View style={styles.resourceContainer}>
+        <GoBackButton />
+      </View>
 
-          <View style={styles.resourceContainer}>
-
-            <GoBackButton/>
-
-            {/* <ActionButton
-              title="Call Navigator"
-              onPress={onClose}
-              buttonStyle={styles.primaryButton}
-              textStyle={styles.primaryButtonText}
-            /> */}
-
-          </View>
-
-        </View>
+    </View>
   );
 };
 
@@ -62,12 +54,12 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
   resourceContainer: {
-    justifyContent: 'center', 
+    justifyContent: 'center',
     alignItems: 'center',
     width: '80%',
   },
   textContainer: {
-    fontSize: 15, 
+    fontSize: 15,
   },
   primaryButton: {
     backgroundColor: '#E2E9F3',
