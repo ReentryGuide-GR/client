75ab8311e2d7dbd0b9fcffd87a44c433c16c4261
harryg02 <94257270+harryg02@users.noreply.github.com>
2024-05-21 09:55:43 -0400

    cleaned up code

--- pages/MainMenu.js
+++ pages/MainMenu.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  StyleSheet, View, Text, Modal, Animated,
+  StyleSheet, View, Text, Animated,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
